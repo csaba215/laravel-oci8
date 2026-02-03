41b224af776505aba93dbde7d11f5747413e1a17
csaba215 <11250204+csaba215@users.noreply.github.com>
2026-02-03 18:35:08 +0000

    fix: pint :robot:

--- tests/Functional/Compatibility/Laravel/DatabaseEloquentHasOneOfManyTest.php
+++ tests/Functional/Compatibility/Laravel/DatabaseEloquentHasOneOfManyTest.php
@@ -83,47 +83,47 @@ class DatabaseEloquentHasOneOfManyTest extends LaravelTestCase
         $this->assertSame('baz', $relation->getRelationName());
     }
 
-//    public function test_correct_latest_of_many_query(): void
-//    {
-//        $user = HasOneOfManyTestUser::create();
-//        $relation = $user->latest_login();
-//        $this->assertSame('select "logins".* from "logins" inner join (select MAX("logins"."id") as "id_aggregate", "logins"."user_id" from "logins" where "logins"."user_id" = ? and "logins"."user_id" is not null group by "logins"."user_id") as "latest_login" on "latest_login"."id_aggregate" = "logins"."id" and "latest_login"."user_id" = "logins"."user_id" where "logins"."user_id" = ? and "logins"."user_id" is not null', $relation->getQuery()->toSql());
-//    }
+    //    public function test_correct_latest_of_many_query(): void
+    //    {
+    //        $user = HasOneOfManyTestUser::create();
+    //        $relation = $user->latest_login();
+    //        $this->assertSame('select "logins".* from "logins" inner join (select MAX("logins"."id") as "id_aggregate", "logins"."user_id" from "logins" where "logins"."user_id" = ? and "logins"."user_id" is not null group by "logins"."user_id") as "latest_login" on "latest_login"."id_aggregate" = "logins"."id" and "latest_login"."user_id" = "logins"."user_id" where "logins"."user_id" = ? and "logins"."user_id" is not null', $relation->getQuery()->toSql());
+    //    }
 
-//    public function test_eager_loading_applies_constraints_to_inner_join_sub_query()
-//    {
-//        $user = HasOneOfManyTestUser::create();
-//        $relation = $user->latest_login();
-//        $relation->addEagerConstraints([$user]);
-//        $this->assertSame('select MAX("logins"."id") as "id_aggregate", "logins"."user_id" from "logins" where "logins"."user_id" = ? and "logins"."user_id" is not null and "logins"."user_id" in (1) group by "logins"."user_id"', $relation->getOneOfManySubQuery()->toSql());
-//    }
+    //    public function test_eager_loading_applies_constraints_to_inner_join_sub_query()
+    //    {
+    //        $user = HasOneOfManyTestUser::create();
+    //        $relation = $user->latest_login();
+    //        $relation->addEagerConstraints([$user]);
+    //        $this->assertSame('select MAX("logins"."id") as "id_aggregate", "logins"."user_id" from "logins" where "logins"."user_id" = ? and "logins"."user_id" is not null and "logins"."user_id" in (1) group by "logins"."user_id"', $relation->getOneOfManySubQuery()->toSql());
+    //    }
 
-//    public function test_global_scope_is_not_applied_when_relation_is_defined_without_global_scope()
-//    {
-//        HasOneOfManyTestLogin::addGlobalScope('test', function ($query) {
-//            $query->orderBy('id');
-//        });
-//
-//        $user = HasOneOfManyTestUser::create();
-//        $relation = $user->latest_login_without_global_scope();
-//        $relation->addEagerConstraints([$user]);
-//        $this->assertSame('select "logins".* from "logins" inner join (select MAX("logins"."id") as "id_aggregate", "logins"."user_id" from "logins" where "logins"."user_id" = ? and "logins"."user_id" is not null and "logins"."user_id" in (1) group by "logins"."user_id") as "latestOfMany" on "latestOfMany"."id_aggregate" = "logins"."id" and "latestOfMany"."user_id" = "logins"."user_id" where "logins"."user_id" = ? and "logins"."user_id" is not null', $relation->getQuery()->toSql());
-//
-//        HasOneOfManyTestLogin::addGlobalScope('test', function ($query) {});
-//    }
+    //    public function test_global_scope_is_not_applied_when_relation_is_defined_without_global_scope()
+    //    {
+    //        HasOneOfManyTestLogin::addGlobalScope('test', function ($query) {
+    //            $query->orderBy('id');
+    //        });
+    //
+    //        $user = HasOneOfManyTestUser::create();
+    //        $relation = $user->latest_login_without_global_scope();
+    //        $relation->addEagerConstraints([$user]);
+    //        $this->assertSame('select "logins".* from "logins" inner join (select MAX("logins"."id") as "id_aggregate", "logins"."user_id" from "logins" where "logins"."user_id" = ? and "logins"."user_id" is not null and "logins"."user_id" in (1) group by "logins"."user_id") as "latestOfMany" on "latestOfMany"."id_aggregate" = "logins"."id" and "latestOfMany"."user_id" = "logins"."user_id" where "logins"."user_id" = ? and "logins"."user_id" is not null', $relation->getQuery()->toSql());
+    //
+    //        HasOneOfManyTestLogin::addGlobalScope('test', function ($query) {});
+    //    }
 
-//    public function test_global_scope_is_not_applied_when_relation_is_defined_without_global_scope_with_complex_query()
-//    {
-//        HasOneOfManyTestPrice::addGlobalScope('test', function ($query) {
-//            $query->orderBy('id');
-//        });
-//
-//        $user = HasOneOfManyTestUser::create();
-//        $relation = $user->price_without_global_scope();
-//        $this->assertSame('select "prices".* from "prices" inner join (select max("prices"."id") as "id_aggregate", min("prices"."published_at") as "published_at_aggregate", "prices"."user_id" from "prices" inner join (select max("prices"."published_at") as "published_at_aggregate", "prices"."user_id" from "prices" where "published_at" < ? and "prices"."user_id" = ? and "prices"."user_id" is not null group by "prices"."user_id") as "price_without_global_scope" on "price_without_global_scope"."published_at_aggregate" = "prices"."published_at" and "price_without_global_scope"."user_id" = "prices"."user_id" where "published_at" < ? group by "prices"."user_id") as "price_without_global_scope" on "price_without_global_scope"."id_aggregate" = "prices"."id" and "price_without_global_scope"."published_at_aggregate" = "prices"."published_at" and "price_without_global_scope"."user_id" = "prices"."user_id" where "prices"."user_id" = ? and "prices"."user_id" is not null', $relation->getQuery()->toSql());
-//
-//        HasOneOfManyTestPrice::addGlobalScope('test', function ($query) {});
-//    }
+    //    public function test_global_scope_is_not_applied_when_relation_is_defined_without_global_scope_with_complex_query()
+    //    {
+    //        HasOneOfManyTestPrice::addGlobalScope('test', function ($query) {
+    //            $query->orderBy('id');
+    //        });
+    //
+    //        $user = HasOneOfManyTestUser::create();
+    //        $relation = $user->price_without_global_scope();
+    //        $this->assertSame('select "prices".* from "prices" inner join (select max("prices"."id") as "id_aggregate", min("prices"."published_at") as "published_at_aggregate", "prices"."user_id" from "prices" inner join (select max("prices"."published_at") as "published_at_aggregate", "prices"."user_id" from "prices" where "published_at" < ? and "prices"."user_id" = ? and "prices"."user_id" is not null group by "prices"."user_id") as "price_without_global_scope" on "price_without_global_scope"."published_at_aggregate" = "prices"."published_at" and "price_without_global_scope"."user_id" = "prices"."user_id" where "published_at" < ? group by "prices"."user_id") as "price_without_global_scope" on "price_without_global_scope"."id_aggregate" = "prices"."id" and "price_without_global_scope"."published_at_aggregate" = "prices"."published_at" and "price_without_global_scope"."user_id" = "prices"."user_id" where "prices"."user_id" = ? and "prices"."user_id" is not null', $relation->getQuery()->toSql());
+    //
+    //        HasOneOfManyTestPrice::addGlobalScope('test', function ($query) {});
+    //    }
 
     public function test_qualifying_sub_select_column()
     {
